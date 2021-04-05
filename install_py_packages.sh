a57ead73b620f1cc513c78e64d6378c406fc52c1
aimld <81764537+aimld@users.noreply.github.com>
2021-04-05 10:27:55 +0100

    Add python libraries script

---
+++ install_py_packages.sh
@@ -0,0 +1,5 @@
+sudo /usr/bin/anaconda/bin/pip install cryptography==3.3.2
+
+sudo /usr/bin/anaconda/bin/pip install azure-storage-blob==12.6.0
+
+sudo /usr/bin/anaconda/bin/pip install azure-storage-file-datalake==12.2.0
